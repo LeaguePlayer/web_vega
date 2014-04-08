a195b10e4a0851e8a86b668f66467ac7c3b64a3c
megakuzmitch <maxro.kuzmitch@gmail.com>
2014-04-08 16:13:01 +0600

    add maintenance page

---
+++ styles/js/script.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function() {
+    /*
+    $('.message-window .close').click(function() {
+        $(this).parents('.message-window').animate({top: -3000}, 500, function() {
+            $(this).remove();
+            $('body').removeClass('blured');
+            $('.overlay').remove();
+        })
+        return false;
+    });
+    */
+});
